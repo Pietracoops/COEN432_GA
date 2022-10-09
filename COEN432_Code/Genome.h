facd80d08ed79eff69100155da3f86c9e43ba7bd
chris7neves <christopher.neves@hotmail.ca>
2022-10-08 20:31:58 -0400

    implemented pmx

--- COEN432_Code/Genome.h
+++ COEN432_Code/Genome.h
@@ -16,7 +16,7 @@ public:
 	void setFitness(int f) { fitness = f; }
 	int getFitness() const { return fitness; }
 	std::string getGenomeString();
-	std::vector<std::vector<int>> getEncoding() const { return genome_encoding_2b2_int; }
+	const std::vector<std::vector<int>>&  getEncoding() const { return genome_encoding_2b2_int; }
 
 	bool operator< (const Genome& other) const { return fitness < other.fitness; }
 	bool operator== (const Genome& rhs) { return (fitness == rhs.fitness) && (genome_encoding_2b2_int == rhs.genome_encoding_2b2_int); }
